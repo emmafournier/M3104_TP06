20431cd0457274fa3dc55b6c53a7c92c32632c53
emmafournier <emma.fournier@etu.univ-grenoble-alpes.fr>
2018-10-20 10:58:02 +0200

    Update model/Format.class.php

--- model/Format.class.php
+++ model/Format.class.php
@@ -1,12 +1,15 @@
 <?php
 
     class Format {
-        public $id;   // Identifiant
-        public $nom;  // nom du format
+        public $idFormat;   // Identifiant
+        public $libelle;  // nom du format
 
-        public function __construct($id,$nom){
-          $this->id = $id;
-          $this->nom = $nom;
+        public function __construct(array $tab = NULL){
+          if($tab != NULL){
+            foreach ($tab as $key => $value) {
+              $this->$key = $value;
+            }
+          }
         }
 
         public function __get($carac) {
