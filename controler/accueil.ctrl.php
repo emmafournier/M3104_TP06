f70e9530b62537d49197fa133e744cd03ddce823
emmafournier <emma.fournier@etu.univ-grenoble-alpes.fr>
2018-10-20 09:46:11 +0200

    Update controler/accueil.ctrl.php

--- controler/accueil.ctrl.php
+++ controler/accueil.ctrl.php
@@ -1,6 +1,18 @@
 <?php
 
+  include_once("../model/DAO.class.php");
 
+  $nbLivres = 5;
 
+  if(isset($_GET['ISBN'])){
+    $isbn = $_GET['ISBN'];
+  }
+  else{
+    $isbn = $dao->firstN(1)[0]->__get("isbn");
+  }
+
+  $livres = $dao->getN($isbn,$nbLivres);
+
+  include("../view/accueil.view.php");
 
 ?>
