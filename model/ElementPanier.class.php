692e08a1656882b3514795f61168918630662dbb
FOURNIER Emma <fouremma@hostname.iut2.upmf-grenoble.fr>
2018-10-24 16:04:32 +0200

    24/10/2018 - utilisateur et panier

---
+++ model/ElementPanier.class.php
@@ -0,0 +1,23 @@
+<?php
+
+    // Un article en vente
+    class ElementPanier {
+        private $idUtilisateur;
+        private $ISBN;
+        private $nb_Exemplaires; 
+
+        public function __construct(array $tab = NULL){
+          if($tab != NULL){
+            foreach ($tab as $key => $value) {
+              $this->$key = $value;
+            }
+          }
+        }
+
+        public function __get($carac) {
+          return $this->$carac;
+        }
+
+    }
+
+?>
