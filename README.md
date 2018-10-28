f0091be6bb436dd805ed83201e72acd29d63dffd
emmafournier <emma.fournier@etu.univ-grenoble-alpes.fr>
2018-10-28 11:31:30 +0100

    28/10/2018 : suppression des fichiers vides !

--- README.md
+++
@@ -1,5 +0,0 @@
-# M3104_TP06
-
-TEST !!
-Test validé ~ Momo
-Validé x 2 cynthia
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+// controleur par défaut de notre site
   header('Location: '.'controler/accueil.ctrl.php');
   session_start();
   $_SESSION['panier'] = array();
